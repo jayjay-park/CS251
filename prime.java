bc9bc9c2c0c446f6874748516cc4783659966b36
park1125 <park1125@purdue.edu>
2022-01-15 18:25:32 -0500

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,27 @@
+public boolean isPrimeSixKSqrt(int n) {
+    compares = 1;
+
+    if (n <= 1) {
+        return false;
+    }
+
+    compares += 1;
+    if (n <= 3) {
+        return true;
+    }
+
+    compares += 2;
+    if (n % 2 == 0 || n % 3 == 0) {
+        return false;
+    }
+
+    for (int i = 5; i * i <= n; i += 6) {
+        compares += 2;
+
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
