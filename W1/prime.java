a2dc5597781478cf602ad5830b00fbc958c7424a
park1125 <park1125@purdue.edu>
2022-01-16 16:17:12 -0500

    prime

--- W1/prime.java
+++ W1/prime.java
@@ -1,27 +1,40 @@
-public boolean isPrimeSixKSqrt(int n) {
-    compares = 1;
+class prime {
 
-    if (n <= 1) {
-        return false;
-    }
+    static int compares = 0;
 
-    compares += 1;
-    if (n <= 3) {
+    static boolean isPrimeSixKSqrt(int n) {
+
+        compares = 1;
+
+        if (n <= 1) {
+            return false;
+        }
+
+        if (n <= 3) {
+            compares += 1;
+            return true;
+        }
+
+
+        if (n % 2 == 0 || n % 3 == 0) {
+            compares += 2;
+            return false;
+        }
+
+        for (int i = 5; i * i <= n; i += 6) {
+            compares += 2;
+
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+
         return true;
     }
 
-    compares += 2;
-    if (n % 2 == 0 || n % 3 == 0) {
-        return false;
+    public static void main(String[] args) {
+
+        isPrimeSixKSqrt(19);
+        System.out.println(compares);
     }
-
-    for (int i = 5; i * i <= n; i += 6) {
-        compares += 2;
-
-        if (n % i == 0 || n % (i + 2) == 0) {
-            return false;
-        }
-    }
-
-    return true;
 }
